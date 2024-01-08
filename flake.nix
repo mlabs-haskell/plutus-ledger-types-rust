fa5d8478f18e0937b0b951a65bc36c74c41df7b7
Szabo Gergely <gege251@mailbox.org>
2024-01-08 10:14:22 +0100

    Add flake-update Hercules CI effect

--- flake.nix
+++ flake.nix
@@ -74,6 +74,19 @@
               inherit cargoArtifacts;
             });
           };
+        hercules-ci.flake-update = {
+          enable = true;
+          updateBranch = "updated-flake-lock";
+          # Next two parameters should always be set explicitly
+          createPullRequest = true;
+          autoMergeMethod = null;
+          when = {
+            # Perform update by Sundays at 12:45
+            minute = 45;
+            hour = 12;
+            dayOfWeek = "Sun";
+          };
+        };
       };
 
 }
