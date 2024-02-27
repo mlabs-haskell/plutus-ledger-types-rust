b48b3c1c88b087ab17bda1d74cc64b98940b892a
Szabo Gergely <gege251@mailbox.org>
2024-02-27 12:26:27 +0100

    From<(TransactionInput, TransactionOutput)> for TxInInfo

--- plutus-ledger-api/src/v1/transaction.rs
+++ plutus-ledger-api/src/v1/transaction.rs
@@ -211,6 +211,21 @@ impl IsPlutusData for TxInInfo {
     }
 }
 
+impl From<(TransactionInput, TransactionOutput)> for TxInInfo {
+    fn from((reference, output): (TransactionInput, TransactionOutput)) -> TxInInfo {
+        TxInInfo { reference, output }
+    }
+}
+
+impl From<(&TransactionInput, &TransactionOutput)> for TxInInfo {
+    fn from((reference, output): (&TransactionInput, &TransactionOutput)) -> TxInInfo {
+        TxInInfo {
+            reference: reference.clone(),
+            output: output.clone(),
+        }
+    }
+}
+
 /// Partial representation of digests of certificates on the ledger.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Hash)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
