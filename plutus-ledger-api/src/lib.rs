e92f2c2aeb32939ed9c4672167a9130bae1fecad
Szabo Gergely <gege251@mailbox.org>
2024-04-29 11:21:13 +0200

    Add deprecation warning

--- plutus-ledger-api/src/lib.rs
+++ plutus-ledger-api/src/lib.rs
@@ -7,6 +7,11 @@ pub mod v2;
 #[cfg(feature = "lbf")]
 pub use lbr_prelude::json;
 pub mod utils;
+#[deprecated(
+    since = "0.2.0",
+    note = "PlutusData should be imported from one of the versioned modules (v1, v2)"
+)]
+pub use v1::plutus_data;
 
 #[macro_use]
 extern crate impl_ops;
