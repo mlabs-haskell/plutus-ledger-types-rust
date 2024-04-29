79bfd392f03fbe8551f452791c5e3bb6de598909
Szabo Gergely <gege251@mailbox.org>
2024-04-29 11:29:32 +0200

    Fix plutus_data deprecation warning

--- plutus-ledger-api/src/lib.rs
+++ plutus-ledger-api/src/lib.rs
@@ -6,12 +6,8 @@ pub mod v1;
 pub mod v2;
 #[cfg(feature = "lbf")]
 pub use lbr_prelude::json;
+pub mod plutus_data;
 pub mod utils;
-#[deprecated(
-    since = "0.2.0",
-    note = "PlutusData should be imported from one of the versioned modules (v1, v2)"
-)]
-pub use v1::plutus_data;
 
 #[macro_use]
 extern crate impl_ops;
---
+++ plutus-ledger-api/src/plutus_data.rs
@@ -0,0 +1,5 @@
+#![deprecated(
+    since = "0.2.0",
+    note = "PlutusData should be imported from one of the versioned modules (v1, v2)"
+)]
+pub use crate::v1::plutus_data::*;
