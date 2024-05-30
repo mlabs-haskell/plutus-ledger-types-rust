0c3cb4384b1bbf5805452e328c06ed11bd0ed033
Szabo Gergely <gege251@mailbox.org>
2024-05-30 15:11:26 +0200

    Update hci-effects

--- hercules-ci.nix
+++ hercules-ci.nix
@@ -28,7 +28,7 @@
                 secretName = "crates-io-token";
                 manifestPath = "${config.packages.plutus-ledger-api-rust-src}/Cargo.toml";
 
-                extraPublishArgs = [ "--dry-run" "--target-dir" "$(mktemp -d)" ];
+                extraPublishArgs = [ "--dry-run" ];
               })
         );
     };
