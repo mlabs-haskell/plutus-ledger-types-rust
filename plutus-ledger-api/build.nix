98c4bd94e92d2138a6be4c8d044acd8d42eb9236
Szabo Gergely <gege251@mailbox.org>
2024-06-17 10:20:58 +0200

    Bump build.nix version

--- plutus-ledger-api/build.nix
+++ plutus-ledger-api/build.nix
@@ -4,6 +4,7 @@
       rustFlake =
         inputs.flake-lang.lib.${system}.rustFlake {
           src = ./.;
+          version = "0.2.0";
           crateName = "plutus-ledger-api";
           devShellHook = config.settings.shell.hook;
           cargoNextestExtraArgs = "--all-features";
